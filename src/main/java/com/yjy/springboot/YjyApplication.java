5f830c9c6a930a634f726aee0a6ac5d6875692d8
jiayin <501023696@qq.com>
2019-07-02 21:33:42 +0800

    首次提交

---
+++ src/main/java/com/yjy/springboot/YjyApplication.java
@@ -0,0 +1,12 @@
+package com.yjy.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YjyApplication {
+	
+	public static void main(String[] args) {
+		SpringApplication.run(YjyApplication.class, args);
+	}
+}
